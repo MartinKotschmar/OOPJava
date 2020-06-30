e75ec7bd1bf81b13259a08ae7fe862d43623ffc9
MartinKotschmar <martin.kotschmar@gmail.com>
2020-06-30 19:35:23 +0200

    WIP

--- src/dev/oopjava/Entitys/Randomizer.java
+++ src/dev/oopjava/Entitys/Randomizer.java
@@ -8,13 +8,22 @@ import java.util.Random;
 
 public class Randomizer {
     private Random r;
+    private int[] randomNumbers;
+    private int i;
 
 
     public Randomizer() {
         r = new Random();
+
     }
 
     public void Generator(){
         //Arrays erstellen, und rausziehen
+
+        i = 0;
+        for(; i < 5; i++) {
+            randomNumbers[i] = r.nextInt();
+
+        }
     }
 }

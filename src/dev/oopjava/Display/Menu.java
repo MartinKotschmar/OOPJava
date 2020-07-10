993fefca2b6c5a889cd0b680bd69e28dd5d7d53b
PuffderZauberdrache <58983942+PuffderZauberdrache@users.noreply.github.com>
2020-07-10 15:35:26 +0200

    Menu fertig

--- src/dev/oopjava/Display/Menu.java
+++ src/dev/oopjava/Display/Menu.java
@@ -2,9 +2,12 @@ package dev.oopjava.Display;
 //Import package
 import dev.oopjava.Display.GameWindow;
 import dev.oopjava.Level.LEVELS;
+import org.w3c.dom.css.RGBColor;
 
 import javax.swing.*;
 import java.awt.*;
+import java.net.MalformedURLException;
+import java.net.URL;
 
 public class Menu {
 
@@ -15,7 +18,7 @@ public class Menu {
     private Dimension dimensionButtonRigidArea;
     private Dimension dimensionButtonSize;
 
-    private JPanel panel;
+    private JLabel panel;
 
     private JButton button1;
     private JButton button2;
@@ -25,8 +28,8 @@ public class Menu {
     public Menu(String title, int width, int height){    //Klasse Display zu erstellen einer Bildschirmabbildung
         this.title = title;     //Titel des Fensters
         this.dimensionWindow = new Dimension(width, height);
-        this.dimensionButtonRigidArea = new Dimension(0,65);
-        this.dimensionButtonSize = new Dimension(900,250);
+        this.dimensionButtonRigidArea = new Dimension(0,165);
+        this.dimensionButtonSize = new Dimension(450,150);
 
         createDisplay();     //create Methode zum eigentlichen erstellen
 
@@ -36,13 +39,21 @@ public class Menu {
         frame = new JFrame(title);
         frame.setSize(dimensionWindow);       //Fenstergröße width, height
         frame.setExtendedState(JFrame.MAXIMIZED_BOTH);      //Vollbild
-        frame.setUndecorated(false);     //Window Toolbar aus
+        frame.setUndecorated(true);     //Window Toolbar aus
         frame.setResizable(false);      //Größe ändern aus
         frame.setLocationRelativeTo(null);      //Zentrieren
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);       //Anwendung bei schließen komplett beenden
 
+        JLabel label = null;
+        try {
+            URL url = new URL("https://i.pinimg.com/originals/e3/eb/31/e3eb311af2c90bd23b9b44d4c8353aa9.gif");
+            label = new JLabel(new ImageIcon(url));
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        }
 
-        panel = new JPanel();
+        //panel = new JPanel();
+        panel = label;
         BoxLayout layout = new BoxLayout(panel, BoxLayout.Y_AXIS);
         panel.setLayout(layout);
 
@@ -54,6 +65,12 @@ public class Menu {
         button1 = new JButton("Spielen");
         button2 = new JButton("Level");
         button3 = new JButton("Beenden");
+
+        button1.setFocusPainted(false);
+
+        button1.setFont(new Font(button1.getFont().getName(), Font.PLAIN, 50));
+        button2.setFont(new Font(button2.getFont().getName(), Font.PLAIN, 50));
+        button3.setFont(new Font(button3.getFont().getName(), Font.PLAIN, 50));
 
         panel.add(Box.createVerticalGlue()); //Platz nach unten
 
@@ -75,6 +92,13 @@ public class Menu {
         button2.setAlignmentX(Component.CENTER_ALIGNMENT);
         button3.setAlignmentX(Component.CENTER_ALIGNMENT);
 
+        button1.setForeground(Color.decode("#09FF00"));
+        button2.setForeground(Color.decode("#09FF00"));
+        button3.setForeground(Color.decode("#09FF00"));
+
+        button1.setBackground(Color.decode("000000"));
+        button2.setBackground(Color.decode("000000"));
+        button3.setBackground(Color.decode("000000"));
 
         // Action Listener auf Buttons machen
         button1.addActionListener(e -> {
@@ -94,12 +118,21 @@ public class Menu {
         frame.pack();
         frame.setVisible(true);     // Display Fenster abbilden
 
+
     }
 
     private void createLevelSelection() {
         frame.remove(panel);
         //frame.setVisible(false);
-        JPanel panel = new JPanel();
+
+
+        JLabel panel = null;
+        try {
+            URL url = new URL("https://i.pinimg.com/originals/e3/eb/31/e3eb311af2c90bd23b9b44d4c8353aa9.gif");
+            panel = new JLabel(new ImageIcon(url));
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        }
 
         BoxLayout layout = new BoxLayout(panel, BoxLayout.Y_AXIS);
         panel.setLayout(layout);
@@ -112,6 +145,18 @@ public class Menu {
         button1 = new JButton("Level1");
         button2 = new JButton("Level2");
         button3 = new JButton("Level3");
+
+        button1.setFont(new Font(button1.getFont().getName(), Font.PLAIN, 50));
+        button2.setFont(new Font(button2.getFont().getName(), Font.PLAIN, 50));
+        button3.setFont(new Font(button3.getFont().getName(), Font.PLAIN, 50));
+
+        button1.setForeground(Color.decode("#09FF00"));
+        button2.setForeground(Color.decode("#09FF00"));
+        button3.setForeground(Color.decode("#09FF00"));
+
+        button1.setBackground(Color.decode("000000"));
+        button2.setBackground(Color.decode("000000"));
+        button3.setBackground(Color.decode("000000"));
 
         panel.add(Box.createVerticalGlue()); //Platz nach unten
 
